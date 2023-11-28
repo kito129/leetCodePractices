a17fea84fc6988165af60dad721a03a738dd559d
kito129 <marco.96.12@gmail.com>
2023-11-28 11:36:41 +0100

    first example

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # leetCodePractices
 A repo where i track and analize my practice in coding with LeetCode
+
+# Top 150 Interview 
+
+- [ ] 88. [Merge Sorted Array](./top150/88.py)
---
+++ top150/88.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def order(self, list_to_order: list[int], len: int) -> None:
+        # simple sorting
+        i=0
+        while(i<len-1):
+            if(list_to_order[i]>list_to_order[i+1]):
+                temp = list_to_order[i]
+                list_to_order[i] = list_to_order[i+1]
+                list_to_order[i+1] = temp
+            i=i+1
+
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # first merge the two array
+        j=0
+        for i in range(n):
+            nums1[i+m] = nums2[i]
+            #nums1[i+(m-1)] = nums2[i]
+        
+        # now sort  in non-decreasing order 
+        self.order(nums1, m+n)
+
+    # to order a single array    
+    
+
+# main
+obj = Solution()
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n=3
+obj.merge(nums1, m, nums2, n)
+print(nums1)
